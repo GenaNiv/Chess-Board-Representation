6354bb1945bf9bfb5bc749766760911fe41491cc
GenaNiv <genadiniv@gmail.com>
2015-11-28 15:54:01 -0400

    Create BoardPrinting.h

---
+++ BoardPrinting.h
@@ -0,0 +1,14 @@
+/*
+ * BoardPrinting.h
+ *
+ *  Created on: Nov 28, 2015
+ *      Author: gena
+ */
+
+#ifndef BOARDPRINTING_H_
+#define BOARDPRINTING_H_
+#define N 8
+//---Functions prototypes----
+void BoardPrinting(char board[][N]);
+
+#endif /* BOARDPRINTING_H_ */
